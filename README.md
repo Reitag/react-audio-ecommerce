3f4b611e9a39835bbe6a131494f8fb39c3c90e0d
Chernov Ilya <ilyacheirnov@gmail.com>
2026-03-29 20:21:56 +0300

    Init react-audio-ecommerce

---
+++ README.md
@@ -0,0 +1,10 @@
+# Audio E-commerce
+
+## Overview
+
+A lightweight React application developed as a technical assessment. It features a curated catalog of audio products and individual product page.
+
+## Tech Stack
+
+- React
+- React Router
---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: ['**/dist', '**/node_modules', '**/build'],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+
+    settings: {
+      react: { version: 'detect' },
+    },
+
+    rules: {
+      // React rules
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+
+      // Hooks rules
+      ...reactHooks.configs.recommended.rules,
+
+      // Vite fast-refresh rule
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // TypeScript rules
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off',
+
+      'no-console': 'off',
+      eqeqeq: ['error', 'always'],
+      'no-unused-vars': 'off',
+
+      // Props
+      'react/prop-types': 'off',
+    },
+  },
+];
